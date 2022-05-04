05d86d555549efad5d6699c4e1e45319367c8e04
NaliniPogula <43030417+NaliniPogula@users.noreply.github.com>
2022-05-04 13:19:35 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HOL_6.Models;
+
+namespace HOL_6.Controllers
+{
+    public class HomeController : Controller
+    {
+        static List<User> users = new List<User>();
+
+        [HttpPost]
+        public ActionResult CreateUser(User u)
+        {
+            users.Add(u);
+            return RedirectToAction("GetAllUsers");
+        }
+
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+
+
+        public ActionResult GetAllUsers()
+        {
+            return PartialView(users);
+        }
+
+        public ActionResult GetServerTime()
+        {
+            System.Threading.Thread.Sleep(5000);
+            return PartialView();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
